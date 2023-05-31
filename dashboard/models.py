4e714829b98a45db54193b5d997f964792f31fad
yerimYeon <99879845+yinneu@users.noreply.github.com>
2023-05-31 18:17:59 +0900

    model change

--- dashboard/models.py
+++ dashboard/models.py
@@ -3,8 +3,8 @@ from django.utils import timezone
 
 
 class UploadedFile(models.Model):
+    user_uuid = models.CharField(primary_key=True, max_length=36)
     file_path = models.CharField(max_length=255)
-    user_uuid = models.CharField(max_length=36)
     uploaded_at = models.DateTimeField(default=timezone.now)
     expiration_time = models.DateTimeField(null=True, blank=True)
 
